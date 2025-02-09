95607438d1d0673d7efa6cf522ff40a3053b5c02
Tushar Kaundal <kaundaltushar@gmail.com>
2025-02-09 13:36:17 +0530

    completed xcounter build out

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((count) => count + 1)}>Increment</button>
+      <button onClick={() => setCount((count) => count - 1)}>Decrement</button>
+    </>
+  );
+}
+
+export default App;
